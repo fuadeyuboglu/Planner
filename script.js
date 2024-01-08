04b6a81cafde193bfab7108754d60563d740f666
Fuad Eyuboglu <fuadeyuboglu@gmail.com>
2024-01-08 18:11:55 +0000

    Created time blocks array of objects. Inserted time block row for each time block item.

--- script.js
+++ script.js
@@ -15,3 +15,78 @@ if (currentDay === '1') {
 // Get the current day element by id and change its inner text
 const currentDayElement = document.getElementById('currentDay');
 currentDayElement.innerText = currentDay;
+
+// Created schedule object
+let timeBlocks = [
+    {
+        "hour": '9AM',
+        "textarea": ''
+    }, 
+
+    {
+        "hour": '10AM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '11AM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '12PM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '1PM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '2PM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '3PM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '4PM',
+        "textarea": ''
+    },
+
+    {
+        "hour": '5PM',
+        "textarea": ''
+    },
+]
+
+// Empty string for time block elements
+let rowString = '';
+
+// Get the container element
+const container = document.querySelector('.container');
+
+// Loop each time block and create a new row
+timeBlocks.forEach(timeBlock => {
+    const row = `
+        <div class="row">
+            <div class="col-1">
+            ${timeBlock.hour}
+            </div>
+            <textarea class="col-10">
+            ${timeBlock.textarea}
+            </textarea>
+            <div class="col-1 saveBtn">
+            </div>
+        </div>
+        `;
+    
+    rowString = rowString + row;
+});
+
+// Implement the time block content into the container element
+container.innerHTML = rowString;
